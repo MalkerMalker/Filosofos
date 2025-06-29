821d1a4f766b0b660c360d6c22eda0906dc81403
Estarossa <rodrigo.manriquez.bastias@gmail.com>
2025-06-29 17:01:00 -0400

    Update filosofos.jl

--- filosofos.jl
+++ filosofos.jl
@@ -1,35 +1,43 @@
 using Base.Threads
 
-const NUM_FILOSOFOS = 5
-const N_COMIDAS_POR_FILOSOFO = 5
-
-const LockType = ReentrantLock
+NUM_FILOSOFOS = 5
+N_COMIDAS_POR_FILOSOFO = 5
+LockType = ReentrantLock
 
 const tenedores = [LockType() for _ in 1:NUM_FILOSOFOS]
 
-const estados_actuales = Dict(i => "iniciando" for i in 1:NUM_FILOSOFOS)
+const estados_actuales = Dict{Int, String}(i => "iniciando" for i in 1:NUM_FILOSOFOS)
 
 mutable struct Filosofo
     id::Int
-    estado::String
     tenedor_izq::Int
     tenedor_der::Int
     comidas_ingeridas::Int
 end
 
-function crear_filosofo(id)
+function crear_filosofo(id::Int)
     tenedor_der = (id % NUM_FILOSOFOS) + 1
-    return Filosofo(id, "pensando", id, tenedor_der, 0)
+    return Filosofo(id, id, tenedor_der, 0)
+end
+
+function actualizar_estado(p::Filosofo, nuevo_estado::String)
+
+    lock(lock_estados) 
+    try
+        estados_actuales[p.id] = nuevo_estado
+    finally
+        unlock(lock_estados) 
+    end
 end
 
 function pensar(p::Filosofo)
-    p.estado = "pensando"
-    estados_actuales[p.id] = p.estado
-    println("Filósofo #$(p.id) está reflexionando sobre la concurrencia...")
+    actualizar_estado(p, "pensando")
+    println("Filósofo #$(p.id) está reflexionando...")
     sleep(rand(0.5:0.1:2.0))
 end
 
 function comer(p::Filosofo)
+
     if p.id == NUM_FILOSOFOS
         lock(tenedores[p.tenedor_der])
         lock(tenedores[p.tenedor_izq])
@@ -39,14 +47,13 @@ function comer(p::Filosofo)
     end
     
     try
-        p.estado = "comiendo"
-        estados_actuales[p.id] = p.estado
         p.comidas_ingeridas += 1
+        actualizar_estado(p, "comiendo")
         println(">> Filósofo #$(p.id) está comiendo por $(p.comidas_ingeridas)ª vez.")
         sleep(rand(0.5:0.1:1.5))
     finally
-        unlock(tenedores[p.tenedor_izq])
         unlock(tenedores[p.tenedor_der])
+        unlock(tenedores[p.tenedor_izq])
         println("Filósofo #$(p.id) ha soltado los tenedores.")
     end
 end
@@ -61,12 +68,11 @@ function ciclo_de_vida_filosofo(id::Int)
         comer(mi_filosofo)
     end
     
-    mi_filosofo.estado = "satisfecho"
-    estados_actuales[mi_filosofo.id] = mi_filosofo.estado
+    actualizar_estado(mi_filosofo, "satisfecho")
     println("== Filósofo #$(mi_filosofo.id) está lleno y se va de la mesa. ==")
 end
 
-println("Bienvenidos a la cena de los filósofos concurrentes.")
+println("Bienvenidos a la cena de los filósofos.")
 
 @threads for i in 1:NUM_FILOSOFOS
     ciclo_de_vida_filosofo(i)
